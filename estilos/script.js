e3e4bd8c616a44bbfb307c38cdb4adec7f3d4078
CesiaMarambio <marambio.cesia@gmail.com>
2023-10-04 19:04:33 -0300

    corregí el formcorrección del formulario clase18

--- estilos/script.js
+++ estilos/script.js
@@ -4,17 +4,31 @@ document.querySelector("button.button-menu-toggle")
                      classList.toggle("nav-links-responsive")
        })
 
-       function validateEmail(email) {
+function validateEmail(email) {
               const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
               return regex.test(email)
             }
             
 function validateForm() {
-       const emailInput = document.getElementById('email');
-       const email = emailInput.value;
-       if (!validateEmail(email)) {
-              alert('Por favor ingrese un correo electrónico válido.');
-       } else {
-              alert('Correo electrónico enviado correctamente.');
+       var nombre = document.getElementById("nombre").value.trim(); 
+       var asunto = document.getElementById("asunto").value.trim();
+       var email = document.getElementById('email').value.trim();
+       
+       console.log(nombre, asunto, email);
+
+
+
+       var nombreTest =/^[a-zA-Z]+$/.test(nombre)
+
+       if(nombreTest === false){
+              alert("Por favor, ingrese un nombre con letras del alfabeto")
+              return false
        }
+
+       if (nombre === "" || asunto === "" || !validateEmail(email)) {
+              alert('Por favor ingrese todos los campos vacío o un correo electrónico válido.');
+              return false
+       } 
+
+       alert('Correo electrónico enviado correctamente. Nos contactaremos con vos en breve. Saludos!!');
 }
