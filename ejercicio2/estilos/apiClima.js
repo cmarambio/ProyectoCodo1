7c123d6b7b496dbed171df83db0668a036cc8140
inerodriguez <145162689+inerodriguez@users.noreply.github.com>
2023-10-18 14:53:48 -0300

    Add files via upload

---
+++ ejercicio2/estilos/apiClima.js
@@ -0,0 +1,42 @@
+
+
+
+const url =  `https://api.weatherapi.com/v1/current.json?key=14edc5fc17294bb7a8121911230709&q=Cordoba`
+
+async function climaCordoba(){
+    try{
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log("Data", data);
+        console.log("Data", typeof(data));
+        console.log("Data", typeof(data.current));
+        const info = data.current;
+        console.log("current", info);
+        console.log(`Cordoba ${info.feelslike_c}°`);
+        console.log(`Icono ${info.condition.icon}`);
+     
+
+        var infoApi = document.querySelector('.infoApi');
+        var imagen = document.querySelector('.imgApiClima');
+        imagen.src = info.condition.icon;
+        console.log(imagen.src);
+        // infoApi.appendChild(imagen)
+        infoApi.innerHTML = `
+        
+        La Temperatura en Unquillo es :
+           ${info.feelslike_c}° , <br>
+           Vientos de  ${info.gust_kph} Kilómetros por Hora,<br>
+           Humedad del  ${info.humidity}%<br>
+           Actualización al ${info.last_updated} hora local<br>
+     `;
+    
+
+
+    }catch(error){
+        console.log("Ocurrió un error", error);
+    }
+
+   
+}
+
+climaCordoba()
